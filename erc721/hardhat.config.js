c389f83605b2099bd9b7285ccb2539c73286e18a
chidubesteve <anikechidube@gmail.com>
2024-10-05 16:29:56 +0100

    minor change

--- erc721/hardhat.config.js
+++ erc721/hardhat.config.js
@@ -5,7 +5,7 @@ const PRIVATE_KEY = vars.get("PRIVATE_KEY")
 module.exports = {
   defaultNetwork: "swisstronik",
   solidity:{
-    version:"0.8.19",
+    version:"0.8.179",
   settings: {
     optimizer: {
       enabled:true,
